e6b96c93b49a7b9afb5c7d09168f51b5f7223261
2695274906 <2695274906@qq.com>
2021-01-26 10:19:25 +0800

    t2

--- java/src/main/java/Calle/copyMap.java
+++ java/src/main/java/Calle/copyMap.java
@@ -5,7 +5,4 @@ public class copyMap {
     public static void main(String[] args) {
         System.out.println("今天是个好天气");
     }
-    public static void main(String[] args) {
-        System.out.println("6666");
-    }
 }
